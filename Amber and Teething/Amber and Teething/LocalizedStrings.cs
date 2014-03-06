953608defb61b30a25202b14bf600d0f3f812719
Keval Langalia <keval.langalia@live.com>
2014-03-06 19:51:51 +0530

    First Commit

---
+++ Amber and Teething/Amber and Teething/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Amber_and_Teething.Resources;
+
+namespace Amber_and_Teething
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
